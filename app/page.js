e9e259ed375cec3a7439afd1a0ab31187e36c885
poulpy-666 <59924437+poulpy-666@users.noreply.github.com>
2026-08-31 13:04:56 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@
 import { useEffect, useMemo, useState } from 'react';
 import Link from 'next/link';
 import { supabase } from './lib/supabase';
+import Header from './composants/Header';
 
 /* =========================================================
    OUTILS
