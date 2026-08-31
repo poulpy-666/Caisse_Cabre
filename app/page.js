6b87765cb39158af6c826582cfc5ed261a73c2d0
poulpy-666 <59924437+poulpy-666@users.noreply.github.com>
2026-08-31 12:41:32 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 'use client';
 
 import { useEffect, useMemo, useState } from 'react';
+import Link from 'next/link';
 import { supabase } from './lib/supabase';
 
 /* =========================================================
